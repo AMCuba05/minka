b80c3632c5a8d01bed87c54c4d4cffa71d5ffa77
Adrian  Cuba <adriancuba0512@gmail.com>
2020-07-04 22:55:51 -0400

    navigation in tab nav added

--- screens/NotificationsOrg.js
+++ screens/NotificationsOrg.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
 export default class NotificationsOrg extends Component {
   render() {
     return (
       <View>
-        <Text>NotificationsXD</Text>
+        <Text>Notification</Text>
+        <Button
+          title={'view'}
+          onPress={() => this.props.navigation.navigate('ViewNotification')}
+        />
       </View>
     );
   }
